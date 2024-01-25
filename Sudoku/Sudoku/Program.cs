32f0bd3c9d71124e3912add3d8b56151375cfb56
Emanuele Quattrocchi <emanuele.quattrocchi@itsincom.it>
2024-01-25 17:21:21 +0100

    Creazione metodo visualizzaMatrice

--- Sudoku/Sudoku/Program.cs
+++ Sudoku/Sudoku/Program.cs
@@ -1,16 +1,33 @@
-﻿public class Program
+﻿using System.Text;
+
+public class Program
 {
     //Matrice che contiene i numeri di gioco
     static int[,] matrice = new int[9, 9];
-
     public static void Main(string[] args)
     {
-
+        visualizzaMatrice();
     }
 
     public static void Matrice()
     {
 
+    }
+    public static void visualizzaMatrice()
+    {
+        Console.WriteLine("Sudoku:");
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while (i != 9)
+        {
+            for (int c = 0; c < 9; c++)
+            {
+                sb.Append(matrice[i, c]).Append(" ");
+            }
+            sb.Append('\n');
+            i++;
+        }
+        Console.WriteLine(Convert.ToString(sb));
     }
 }
 
