881075f7422def91ab15ceea9191036d166a7493
Mattia Marassi <matticila004@gmail.com>
2024-01-25 16:51:22 +0100

    Aggiunto inserimento

--- Sudoku/Sudoku/Program.cs
+++ Sudoku/Sudoku/Program.cs
@@ -5,12 +5,19 @@
 
     public static void Main(string[] args)
     {
-
+        
     }
 
-    public static void Matrice()
+    public static bool Inserisci(int x, int y, int valore)
     {
+        if (x < 0 && x > 8 || y < 0 && y > 8)
+        {
+            Console.WriteLine("Posizione non esistente");
+            return false;
+        }
 
+        matrice[x, y] = valore;
+        return true;
     }
 }
 
